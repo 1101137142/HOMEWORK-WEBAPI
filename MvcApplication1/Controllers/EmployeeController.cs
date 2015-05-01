9cc3c60eb0e13dc711c310318311c114433c0fda
1101137142 <1101137142@gm.kuas.edu.tw>
2015-05-01 16:19:07 +0800

    WebApi

---
+++ MvcApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using KuasCore.Models;
+using KuasCore.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MvcApplication1.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+
+        public IEmployeeService EmployeeService { get; set; }
+
+        [HttpGet]
+        public IList<Employee> GetAllEmployees()
+        {
+            return EmployeeService.GetAllEmployees();
+        }
+    }
+}
